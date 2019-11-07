bc6ed3ce9bac5e4bf524b2d548453c00f7fbca5e
kacper <kjabcon@gmail.com>
2019-11-07 10:11:50 +0100

    form extended with calendar and description

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addTask } from '../actions';
 import { Button, Typography, TextField, Container, FormControl } from '@material-ui/core';
+import Calendar from './Calendar'
+
+
 
 const formStyle = {
     display: 'flex',
@@ -9,6 +12,7 @@ const formStyle = {
     alignItems: 'center',
     flexDirection: 'columns',
 }
+
 
 
 class Form extends Component {
@@ -23,23 +27,52 @@ class Form extends Component {
         const { name, value } = e.target
         this.setState({ [name]: value });
     }
+
     onSubmit = (e) => {
-
-
         e.preventDefault();
         const taskItem = {
             task: this.state.task,
         };
         this.props.addTask(taskItem);
     }
+
     render() {
         const { task } = this.state;
-
         return (
             <Container>
                 <FormControl style={formStyle} >
                     <Typography variant='h5'>Create new task</Typography>
-                    <TextField type='text' name='task' onChange={this.onChange} value={task}></TextField>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        width: '35rem',
+                        marginTop: '30px'
+                    }}>
+                        <TextField
+                            label="Title"
+                            id="outlined-margin-dense"
+                            margin="dense"
+                            variant="outlined"
+                            onChange={this.onChange}
+                            defaultValue={task}
+                            type='title'
+                            name='task'
+                        />
+                        <Calendar />
+                    </div>
+                    <TextField
+                        id="outlined-full-width"
+                        label="Description"
+                        style={{ margin: 8, width: '35rem' }}
+                        placeholder="Type task description"
+
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        variant="outlined"
+                    />
                     <br />
                     <Button onClick={this.onSubmit} color='primary' variant='contained'>Submit</Button>
                 </FormControl>
